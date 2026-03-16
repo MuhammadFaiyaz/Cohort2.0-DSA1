4b98cbd59c7c5da338ad5116787957a0de217f6f
Faiyaz <mk3590653@gmail.com>
2026-03-16 12:35:58 +0600

    DSA programming with multi-dimenshional array

--- app11.js
+++ app11.js
@@ -46,13 +46,64 @@ let matrix = [
 ];
 let rows = matrix.length;
 for (let i = 0; i < rows; i++) {
-  for (let j = i+1; j < rows; j++) {
+  for (let j = i + 1; j < rows; j++) {
     let temp = matrix[i][j];
     matrix[i][j] = matrix[j][i];
     matrix[j][i] = temp;
   }
 }
-for(let i = 0; i < matrix.length; i++){
-    matrix[i].reverse()
+for (let i = 0; i < matrix.length; i++) {
+  matrix[i].reverse();
 }
 console.log(matrix);
+
+// set matrix zeroes
+let matrix2 = [
+  [1, 2, 3],
+  [4, 0, 6],
+  [7, 8, 9],
+];
+let firstColZero = false;
+for (let i = 0; i < matrix2.length; i++) {
+  if (matrix2[i][0] === 0) {
+    firstColZero = true;
+  }
+  for (let j = 1; j < matrix2[0].length; j++) {
+    if (matrix2[i][j] === 0) {
+      matrix2[i][0] = 0;
+      matrix2[0][j] = 0;
+    }
+  }
+}
+for (let i = 1; i < matrix2.length; i++) {
+  for (let j = 1; j < matrix2[0].length; j++) {
+    if (matrix2[i][0] === 0 || matrix2[0][j] === 0) {
+      matrix2[i][j] = 0;
+    }
+  }
+}
+if (matrix2[0][0] === 0) {
+  for (let i = 0; i < matrix2[0].length; i++) {
+    matrix2[0][i] = 0;
+  }
+}
+if (firstColZero) {
+  for (let i = 0; i < matrix2.length; i++) {
+    matrix2[i][0] = 0;
+  }
+}
+
+console.log(matrix2);
+
+let arr = [[10, 20], [30, 40]];
+arr[0][1] = arr[1][0];
+console.log(arr[0][1]);
+
+const mySet = new Set();
+mySet.add(1);
+mySet.add(1);
+console.log(mySet.size);
+
+const set = new Set([1, 2, 3]);
+set.clear();
+console.log(set.size);
