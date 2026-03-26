27e091b7200b5b980846016a0af4fa2fc0f7a280
Faiyaz <mk3590653@gmail.com>
2026-03-26 17:37:00 +0600

    hashing problem

--- app14.js
+++ app14.js
@@ -15,26 +15,72 @@
 // console.log(result);
 
 // Distribute candies
-let n = 7;
-let k = 3;
-let candies = [1, 1, 2, 2, 3, 3, 4];
+// let n = 7;
+// let k = 3;
+// let candies = [1, 1, 2, 2, 3, 3, 4];
 
+// let freq = {};
+// for (let candy of candies) {
+//   freq[candy] = (freq[candy] || 0) + 1;
+// }
+
+// let uniqueTypes = Object.keys(freq).length;
+// if (uniqueTypes < k) return 0;
+// let maxChildren = Math.floor(n / k);
+// for (let children = maxChildren; children >= 1; children--) {
+//   let typesAvailable = 0;
+//   for (const type in freq) {
+//     typesAvailable += Math.floor(freq[type] / children);
+//   }
+//   if (typesAvailable >= k) {
+//     return (children);
+//   }
+// }
+
+// kth distinct string in array
+// let arr = ["a", "b", "c", "d", "a", "b", "e"];
+// let k = 2;
+// let freq = {};
+// for (const elem of arr) {
+//   freq[elem] = (freq[elem] || 0) + 1;
+// }
+// let distinct = [];
+// for (const elem of arr) {
+//   if(freq[elem]===1)  distinct.push(elem)
+// }
+// if(distinct.length>=k) console.log(distinct[k-1])
+// else console.log('None')
+
+//  pattern word
+let pattern = "abba";
+let s = "dog cat cat dog";
+let words = s.split(" ");
+if (pattern.length !== words.length) console.log(false);
+
+let map1 = {};
+let map2 = {};
+for (let i = 0; i < words.length; i++) {
+  let p = pattern[i];
+  let w = words[i];
+  if (map1[p] && map1[p] !== w) return false;
+  if (map2[w] && map2[w] !== p) return false;
+
+  map1[p] = w;
+  map2[w] = p;
+}
+// return true
+
+// set mismatch
+let nums = [2, 2];
 let freq = {};
-for (let candy of candies) {
-  freq[candy] = (freq[candy] || 0) + 1;
-}
 
-let uniqueTypes = Object.keys(freq).length;
-if (uniqueTypes < k) return 0;
-let maxChildren = Math.floor(n / k);
-for (let children = maxChildren; children >= 1; children--) {
-  let typesAvailable = 0;
-  for (const type in freq) {
-    typesAvailable += Math.floor(freq[type] / children);
+for (let num of nums) {
+    freq[num] = (freq[num] || 0) + 1;
   }
-  if (typesAvailable >= k) {
-    return (children);
-  }
+let duplicate = -1;
+let missing = -1;
+for (let i = 1; i <= nums.length; i++) {
+  if(freq[i]===2) duplicate = i;
+  if(!freq[i]) missing = i
 }
-
-
+console.log(duplicate, missing)
